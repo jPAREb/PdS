bd9b6e8d60ea2e83bb4bf1f11d1416e238789670
Victorgg1993 <vigomga1993@gmail.com>
2020-07-15 02:35:39 +0200

    Optimització cutre en el codi
    No se què faig fent això, hauria d'estar dormint però bueno.

--- Banderes.h
+++ Banderes.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <windows.h>
 #include <stdio.h>
+
+#define COLOR_VERMELL BACKGROUND_RED
+#define COLOR_GROC (COLOR_VERMELL | BACKGROUND_GREEN)
+
 void color_lletres(WORD* Attributes, DWORD Colour)
 {
     CONSOLE_SCREEN_BUFFER_INFO Info;
@@ -13,24 +17,19 @@ void color_lletres(WORD* Attributes, DWORD Colour)
 void bandera_senyera()
 {
     WORD Attributes = 0;
-    color_lletres(&Attributes, BACKGROUND_INTENSITY | BACKGROUND_RED | BACKGROUND_GREEN);
-    cout << "                                          \n";
-    color_lletres(&Attributes, BACKGROUND_INTENSITY | BACKGROUND_RED);
-    cout << "                                          \n";
-    color_lletres(&Attributes, BACKGROUND_INTENSITY | BACKGROUND_RED | BACKGROUND_GREEN);
-    cout << "                                          \n";
-    color_lletres(&Attributes, BACKGROUND_INTENSITY | BACKGROUND_RED);
-    cout << "                                          \n";
-    color_lletres(&Attributes, BACKGROUND_INTENSITY | BACKGROUND_RED | BACKGROUND_GREEN);
-    cout << "                                          \n";
-    color_lletres(&Attributes, BACKGROUND_INTENSITY | BACKGROUND_RED);
-    cout << "                                          \n";
-    color_lletres(&Attributes, BACKGROUND_INTENSITY | BACKGROUND_RED | BACKGROUND_GREEN);
-    cout << "                                          \n";
-    color_lletres(&Attributes, BACKGROUND_INTENSITY | BACKGROUND_RED);
-    cout << "                                          \n";
-    color_lletres(&Attributes, BACKGROUND_INTENSITY | BACKGROUND_RED | BACKGROUND_GREEN);
-    cout << "                                          \n";
+    
+    for(int i = 0; i < 5; i++) // es pinten les línies grogues i vermelles de cop, excepte en l'últim cas
+    {
+        color_lletres(&Attributes, BACKGROUND_INTENSITY | COLOR_GROC);
+        cout << "                                          \n";
+        
+        if( i != 4) // en l'últim cas no es pinta el vermell
+        {
+            color_lletres(&Attributes, BACKGROUND_INTENSITY | COLOR_VERMELL);
+            cout << "                                          \n";
+        }
+    }
+
     Sleep(2000);
     color_lletres(&Attributes, FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN);
 }
@@ -38,23 +37,25 @@ void bandera_senyera()
 void bandera_estanquera()
 {
     WORD Attributes = 0;
-    color_lletres(&Attributes, BACKGROUND_INTENSITY | BACKGROUND_RED);
-    cout << "                                          \n";
-    color_lletres(&Attributes, BACKGROUND_INTENSITY | BACKGROUND_RED);
-    cout << "                                          \n";
-    color_lletres(&Attributes, BACKGROUND_INTENSITY | BACKGROUND_RED | BACKGROUND_GREEN);
-    cout << "                                          \n";
-    color_lletres(&Attributes, BACKGROUND_INTENSITY | BACKGROUND_RED | BACKGROUND_GREEN);
-    cout << "                                          \n";
-    color_lletres(&Attributes, BACKGROUND_INTENSITY | BACKGROUND_RED | BACKGROUND_GREEN);
-    cout << "                                          \n";
-    color_lletres(&Attributes, BACKGROUND_INTENSITY | BACKGROUND_RED | BACKGROUND_GREEN);
-    cout << "                                          \n";
-    color_lletres(&Attributes, BACKGROUND_INTENSITY | BACKGROUND_RED);
-    cout << "                                          \n";
-    color_lletres(&Attributes, BACKGROUND_INTENSITY | BACKGROUND_RED);
-    cout << "                                          \n";
-    color_lletres(&Attributes, BACKGROUND_INTENSITY | BACKGROUND_RED);
+    
+    for(int i = 0; i < 2; i++) // primera línia vermella
+    {
+        color_lletres(&Attributes, BACKGROUND_INTENSITY | COLOR_VERMELL);
+        cout << "                                          \n";
+    }
+    
+    for(int i = 0; i < 4; i++) // primera (i única) línia groca
+    {
+        color_lletres(&Attributes, BACKGROUND_INTENSITY | COLOR_GROC);
+        cout << "                                          \n";
+    }
+
+    for(int i = 0; i < 3; i++) // segona ( i última ) vermella
+    {
+        color_lletres(&Attributes, BACKGROUND_INTENSITY | COLOR_VERMELL);
+        cout << "                                          \n";
+    }
+        
     Sleep(2000);
     color_lletres(&Attributes, FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN);
 }
